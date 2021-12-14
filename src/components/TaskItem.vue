19355cd29437b2ee5cf80ac3984e077fff656377
Nikolay Zverev <user@mail.ru>
2021-12-14 14:39:27 +0300

    Create todo list

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <li class="tasks-item">
+    <input type="checkbox" @change="$emit('complete')" :checked="task.completed" :id="'task-' + task.id" name="check" />
+    <label :for="'task-' + task.id">{{task.title}}</label>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    task: Object
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  label {
+    position: relative;
+    margin-left: 35px;
+    cursor: pointer;
+    color: #fff;
+    font-weight: 500;
+    font-size: 16px;
+  }
+  input[type=checkbox] {
+    display: none;
+  }
+  input[type=checkbox] + label:before {
+    position: absolute;
+    content: "";
+    left: -35px;
+    top: calc(50% - 10px);
+    display: inline-block;
+    border-radius: 6px;
+    font-size: 20px;
+    line-height: 22px;
+    background: #4F5565;
+    height: 20px;
+    width: 20px;
+    text-align: center;
+    vertical-align: middle;
+    transition: color ease .3s;
+    margin-right: 15px;
+    cursor: pointer;
+  }
+  input[type=checkbox]:checked + label:before{
+    background: #FF8469;
+  }
+  input[type=checkbox]:checked + label:after {
+    position: absolute;
+    content: '';
+    top: calc(50% - 8px);
+    left: -29px;
+    width: 6px;
+    height: 10px;
+    transform: rotate(40deg);
+    border-right: 2px solid #fff;
+    border-bottom: 2px solid #fff;
+    color: #fff;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+  }
+  input[type=checkbox]:checked + label {
+    text-decoration: line-through;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="todo-list">
+    <div class="first-block"></div>
+    <div class="second-block"></div>
+    <div class="card">
+      <div class="header">
+        <h1>Todo list</h1>
+        <button>Add</button>
+      </div>
+      <div>
+        <transition-group name="fade" tag="ul" class="tasks">
+          <task-item v-for="task in tasks"
+                     @complete="completeTask(task)"
+                     :task="task"
+                     :key="task.id"></task-item>
+        </transition-group>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+import TaskItem from './TaskItem'
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      tasks: null,
+      loading: true,
+      errored: false
+    }
+  },
+  components: {
+    TaskItem
+  },
+  mounted() {
+    axios
+        .get('https://jsonplaceholder.typicode.com/todos')
+        .then(response => {
+          this.tasks = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+  },
+  methods: {
+    completeTask(task) {
+      task.completed = !task.completed;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .todo-list {
+    position: relative;
+    .first-block {
+      position: absolute;
+      top: -20px;
+      left: 1px;
+      z-index: 40;
+      width: 395px;
+      height: 68px;
+      background: #31394D;
+      border-radius: 20px;
+      transform: rotate(-2deg);
+    }
+    .second-block {
+      position: absolute;
+      top: -31px;
+      left: 3px;
+      z-index: 30;
+      width: 341px;
+      height: 85px;
+      border-radius: 20px;
+      background: #4F5565;
+      transform: rotate(-4deg);
+    }
+    .card {
+      width: 420px;
+      height: auto;
+      border-radius: 20px;
+      background: #252E42;
+      padding: 30px;
+      position: relative;
+      z-index: 50;
+      .header {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 30px;
+        h1 {
+          margin: 0;
+          font-weight: bold;
+          color: #fff;
+        }
+        button {
+          background: #FF8469;
+          font-weight: 500;
+          border: none;
+          outline: none;
+          color: #fff;
+          padding: 5px 10px;
+          border-radius: 7px;
+          cursor: pointer;
+          text-transform: uppercase;
+        }
+      }
+      .tasks-item {
+        display: flex;
+        margin-bottom: 18px;
+      }
+    }
+  }
+
+</style>
